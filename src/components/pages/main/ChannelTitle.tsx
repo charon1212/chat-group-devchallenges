340277da2aee4d169f3459a89eb18cebab0f4dee
charon1212 <charons2turquoise@gmail.com>
2022-06-26 20:05:59 +0900

    update.

--- src/components/pages/main/ChannelTitle.tsx
+++ src/components/pages/main/ChannelTitle.tsx
@@ -1,8 +1,13 @@
+import { useAppSelector } from '../../../app/hooks';
+import { selectChannel } from '../../../features/channel/channelSlice';
+
 const ChannelTitle = () => {
+  const channel = useAppSelector(selectChannel);
+
   return (
     <>
       <div style={{ boxShadow: '0px 1px 10px 0px #000', border: '1px solid #000' }}>
-        <h3>FRONT-END DEVELOPERS</h3>
+        <h3>{channel.title}</h3>
       </div>
     </>
   );
--- src/components/pages/main/MainContent.tsx
+++ src/components/pages/main/MainContent.tsx
@@ -1,15 +1,24 @@
+import { useAppSelector } from '../../../app/hooks';
+import { selectChannel } from '../../../features/channel/channelSlice';
 import ChannelTitle from './ChannelTitle';
 import ChatFrame from './ChatFrame';
 import MessageSender from './MessageSender';
 
 const MainContent = () => {
+  const channel = useAppSelector(selectChannel);
 
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column', width: '100%', backgroundColor: '#282828' }}>
-        <ChannelTitle />
-        <ChatFrame />
-        <MessageSender />
+        {channel.uid ? (
+          <>
+            <ChannelTitle />
+            <ChatFrame />
+            <MessageSender />
+          </>
+        ) : (
+          ''
+        )}
       </div>
     </>
   );

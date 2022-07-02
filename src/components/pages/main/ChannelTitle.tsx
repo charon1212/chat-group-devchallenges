466d47c9b9895b35cb584a1a00a3000d29a9e2e3
charon1212 <charons2turquoise@gmail.com>
2022-07-02 10:28:54 +0900

    update margin

--- src/components/pages/main/ChannelTitle.tsx
+++ src/components/pages/main/ChannelTitle.tsx
@@ -1,12 +1,14 @@
+import { useTheme } from '@mui/material';
 import { useAppSelector } from '../../../app/hooks';
 import { selectChannel } from '../../../features/channel/channelSlice';
 
 const ChannelTitle = () => {
   const channel = useAppSelector(selectChannel);
+  const theme = useTheme();
 
   return (
     <>
-      <div style={{ boxShadow: '0px 1px 10px 0px #000', border: '1px solid #000' }}>
+      <div style={{ margin: theme.spacing(0, 0, 0, 4), boxShadow: '0px 1px 10px 0px #000', border: '1px solid #000' }}>
         <h3>{channel.title}</h3>
       </div>
     </>

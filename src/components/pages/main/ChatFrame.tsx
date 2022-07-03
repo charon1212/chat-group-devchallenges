8599d30ad210ad9cd6ee78b25fe6ea0075722a83
charon1212 <charons2turquoise@gmail.com>
2022-07-03 23:10:02 +0900

    show chat date.

--- src/components/pages/main/ChatFrame.tsx
+++ src/components/pages/main/ChatFrame.tsx
@@ -11,7 +11,7 @@ const ChatFrame = () => {
   const theme = useTheme();
   const { ref } = useScrollAt();
   const channel = useAppSelector(selectChannel);
-  const [chatList, setChatList] = useState<Chat[]>([]);
+  const [chatList, setChatList] = useState<(Chat & { dateObj: DateObj })[]>([]);
   useEffect(() => {
     myFirestoreKitChat.listenCollection({ channel }, async (list) => {
       await Promise.all(
@@ -19,7 +19,7 @@ const ChatFrame = () => {
           v.user = (await myFirestoreKitUser.get({}, v.user.uid))!;
         })
       );
-      setChatList(list.sort((a, b) => a.dateMilliseconds - b.dateMilliseconds));
+      setChatList(list.sort((a, b) => a.dateMilliseconds - b.dateMilliseconds).map((v) => ({ ...v, dateObj: getDateObj(v.dateMilliseconds) })));
     });
   }, [channel]);
 
@@ -28,10 +28,16 @@ const ChatFrame = () => {
       <div style={{ height: 'auto', overflowY: 'hidden' }}>
         <div style={{ margin: theme.spacing(1, 3, 1, 1), overflowY: 'scroll', maxHeight: '100%' }}>
           <List>
-            {chatList.map((chat) => (
-              <ChatBox chat={chat} />
-            ))}
-            <DateDivider />
+            {chatList.map((chat, i) => {
+              const postChat = chatList[i + 1];
+              const setDivider = !postChat || !sameDay(postChat.dateObj, chat.dateObj);
+              return (
+                <>
+                  <ChatBox chat={chat} />
+                  {setDivider ? <DateDivider dateObj={chat.dateObj} /> : ''}
+                </>
+              );
+            })}
             <div ref={ref} />
           </List>
         </div>
@@ -40,22 +46,26 @@ const ChatFrame = () => {
   );
 };
 
-type PropsChatBox = { chat: Chat };
+type PropsChatBox = { chat: Chat & { dateObj: DateObj } };
 const ChatBox = (props: PropsChatBox) => {
   const { chat } = props;
+  const { hour, minute, second } = chat.dateObj;
+  const dateString = `${hour}:${minute}:${second}`;
   return (
     <>
       <ListItem alignItems='flex-start'>
         <ListItemAvatar>
           <Avatar alt='avatar-image' src={chat.user.photoUrl} />
         </ListItemAvatar>
-        <ListItemText primary={chat.user.displayName} secondary={chat.chatContent} />
+        <ListItemText primary={`${chat.user.displayName}`} secondary={`${chat.chatContent} - ${dateString}`} />
       </ListItem>
     </>
   );
 };
 
-const DateDivider = () => {
+type PropsDivider = { dateObj: DateObj };
+const DateDivider = (props: PropsDivider) => {
+  const { dateObj } = props;
   return (
     <>
       <table>
@@ -70,7 +80,7 @@ const DateDivider = () => {
               <Divider />
             </td>
             <td>
-              <div style={{ marginLeft: '20px', marginRight: '20px' }}>2020/05/01</div>
+              <div style={{ marginLeft: '20px', marginRight: '20px' }}>{`${dateObj.year}/${dateObj.month}/${dateObj.day}`}</div>
             </td>
             <td>
               <Divider />
@@ -80,6 +90,22 @@ const DateDivider = () => {
       </table>
     </>
   );
+};
+
+const sameDay = (d1: DateObj, d2: DateObj) => d1.year === d2.year && d1.month === d2.month && d1.day === d2.day;
+
+type DateObj = ReturnType<typeof getDateObj>;
+const getDateObj = (m: number) => {
+  const d = new Date(m);
+  return {
+    year: d.getFullYear(),
+    month: d.getMonth() + 1,
+    day: d.getDate(),
+    hour: d.getHours(),
+    minute: d.getMinutes(),
+    second: d.getSeconds(),
+    milliSeconds: d.getMilliseconds(),
+  };
 };
 
 export default ChatFrame;

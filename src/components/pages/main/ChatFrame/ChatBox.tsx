bfd8dc07d1f6465fb23bfced3708fb538994b8a1
charon1212 <charons2turquoise@gmail.com>
2022-07-24 21:21:01 +0900

    fix ChatBox implementation

--- src/components/pages/main/ChatFrame/ChatBox.tsx
+++ src/components/pages/main/ChatFrame/ChatBox.tsx
@@ -1,10 +1,22 @@
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { Chat } from "../../../../domain/type/Chat";
 import { DateObj } from "./DateObj";
 
 type Props = { chat: Chat & { dateObj: DateObj } };
 const ChatBox = (props: Props) => {
-  const {} = props;
-  return <></>;
+  const { chat } = props;
+  const { hour, minute, second } = chat.dateObj;
+  const dateString = `${hour}:${minute}:${second}`;
+  return (
+    <>
+      <ListItem alignItems='flex-start'>
+        <ListItemAvatar>
+          <Avatar alt='avatar-image' src={chat.user.photoUrl} />
+        </ListItemAvatar>
+        <ListItemText primary={`${chat.user.displayName}`} secondary={`${chat.chatContent} - ${dateString}`} />
+      </ListItem>
+    </>
+  );
 };
 
 export default ChatBox;
